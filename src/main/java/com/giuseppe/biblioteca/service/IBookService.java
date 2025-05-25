c66b85353a86b800fe7ebe060ad2e078a4ef07fc
Durren-it <micheledelvescovo@gmail.com>
2025-05-25 21:25:00 +0200

    Aggiunta chiamate alle funzioni del repository in service interface

--- src/main/java/com/giuseppe/biblioteca/service/IBookService.java
+++ src/main/java/com/giuseppe/biblioteca/service/IBookService.java
@@ -10,4 +10,11 @@ public interface IBookService {
     BookDTO createBook(BookDTO bookDTO);
     BookDTO updateBook(Long id, BookDTO bookDTO);
     boolean deleteBook(Long id);
+    List<BookDTO> findBooksByAuthor(String author);
+    List<BookDTO> findBooksByGenre(String genre);
+    List<BookDTO> searchBooksByTitle(String title);
+    List<BookDTO> findBooksByAnnoLessThan(int year);
+    int countBooksByAuthor(String author);
+    List<BookDTO> getBooksSortedByAnnoDesc();
+    List<BookDTO> findBooksByTitleOrAuthor(String title, String author);
 }
